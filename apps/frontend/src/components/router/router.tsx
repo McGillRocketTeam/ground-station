f4c48513d004cae085b0d07431f224069b0415b3
Léo <hello@leomindlin.com>
2026-01-31 12:15:00 -0500

    feat(frontend): dashboard view (#2)

--- apps/frontend/src/components/router/router.tsx
+++ apps/frontend/src/components/router/router.tsx
@@ -1,12 +1,13 @@
 import { createBrowserRouter } from "react-router";
 import { RootErrorBoundary } from "./error-boundary";
 import { DebugPage } from "@/pages/debug";
+import { DashboardPage } from "@/pages/dashboard";
 
 export const router = createBrowserRouter([
   {
     errorElement: <RootErrorBoundary />,
     children: [
-      { path: "/", element: <div>Hello World!</div> },
+      { path: "/", element: <DashboardPage /> },
       {
         path: "/debug",
         element: <DebugPage />,

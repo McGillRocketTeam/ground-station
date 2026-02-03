4ed6e3dd97d12133a0560032d443b51bba3da5bc
Léo Mindlin <hello@leomindlin.com>
2026-02-02 21:16:08 -0500

    working chart card

--- packages/yamcs-effect/src/http/index.ts
+++ packages/yamcs-effect/src/http/index.ts
@@ -2,9 +2,11 @@ import { HttpApi } from "@effect/platform";
 import commandGroup from "./command.js";
 import linkGroup from "./link.js";
 import mdbGroup from "./mdb.js";
+import parameterGroup from "./parameter.js";
 
 export const YamcsApi = HttpApi.make("YAMCS")
   .add(mdbGroup)
   .add(commandGroup)
   .add(linkGroup)
+  .add(parameterGroup)
   .prefix("/api");

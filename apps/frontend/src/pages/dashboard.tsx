c7962891691bc60697b853b8b803be9ba140ae6b
Léo Mindlin <hello@leomindlin.com>
2026-02-05 00:18:03 -0500

    layout saving

--- apps/frontend/src/pages/dashboard.tsx
+++ apps/frontend/src/pages/dashboard.tsx
@@ -1,12 +1,20 @@
-import { DockviewReact, themeAbyssSpaced } from "dockview-react";
+import {
+  DockviewApi,
+  DockviewReact,
+  themeAbyssSpaced,
+  type DockviewReadyEvent,
+  type SerializedDockview,
+} from "dockview-react";
 
 import { DashboardPlus } from "@/components/dashboard/dashboard-plus";
 import { DashboardTab } from "@/components/dashboard/dashboard-tab";
 import { CardComponentMap } from "@/lib/cards";
 import { formatDate } from "@/lib/utils";
-import { useAtomSuspense } from "@effect-atom/atom-react";
+import { Atom, useAtom, useAtomSuspense } from "@effect-atom/atom-react";
+import { BrowserKeyValueStore } from "@effect/platform-browser";
 import { timeSubscriptionAtom } from "@mrt/yamcs-atom";
-import { Suspense } from "react";
+import { Schema } from "effect";
+import { Suspense, useEffect, useState } from "react";
 import "./dashboard.css";
 
 function Time() {
@@ -31,7 +39,68 @@ function MissionTime() {
   );
 }
 
+const runtime = Atom.runtime(BrowserKeyValueStore.layerLocalStorage);
+
+const dashboardLocalStorage = Atom.kvs({
+  runtime: runtime,
+  key: "mrt-dashboard",
+  schema: Schema.Object,
+  defaultValue: () => ({}),
+});
+
 export function DashboardPage() {
+  const [api, setApi] = useState<DockviewApi>();
+  const [layout, setLayout] = useAtom(dashboardLocalStorage);
+
+  useEffect(() => {
+    if (!api) {
+      return;
+    }
+
+    const disposable = api.onDidLayoutChange(() => {
+      const layout: SerializedDockview = api.toJSON();
+      setLayout(layout);
+    });
+
+    return () => disposable.dispose();
+  }, [api]);
+
+  const onReady = (event: DockviewReadyEvent) => {
+    setApi(event.api);
+
+    try {
+      event.api.fromJSON(layout as SerializedDockview);
+    } catch (err) {
+      console.error("Error loading layout", err);
+
+      event.api.addPanel({
+        title: "Parameter Table",
+        component: "parameter-table",
+        id: crypto.randomUUID(),
+      });
+      event.api.addPanel({
+        title: "Command History",
+        component: "command-history",
+        id: crypto.randomUUID(),
+      });
+      event.api.addPanel({
+        title: "Parameter Chart",
+        component: "parameter-chart",
+        id: crypto.randomUUID(),
+      });
+      event.api.addPanel({
+        title: "Events",
+        component: "events",
+        id: crypto.randomUUID(),
+      });
+      event.api.addPanel({
+        title: "Links",
+        component: "links",
+        id: crypto.randomUUID(),
+      });
+    }
+  };
+
   return (
     <div className="fixed flex h-full w-full flex-col gap-1.25 p-1.25">
       <div className="flex flex-row justify-between">
@@ -43,33 +112,7 @@ export function DashboardPage() {
       </div>
       <div className="grow">
         <DockviewReact
-          onReady={(event) => {
-            event.api.addPanel({
-              title: "Parameter Table",
-              component: "parameter-table",
-              id: crypto.randomUUID(),
-            });
-            event.api.addPanel({
-              title: "Command History",
-              component: "command-history",
-              id: crypto.randomUUID(),
-            });
-            event.api.addPanel({
-              title: "Parameter Chart",
-              component: "parameter-chart",
-              id: crypto.randomUUID(),
-            });
-            event.api.addPanel({
-              title: "Events",
-              component: "events",
-              id: crypto.randomUUID(),
-            });
-            event.api.addPanel({
-              title: "Links",
-              component: "links",
-              id: crypto.randomUUID(),
-            });
-          }}
+          onReady={onReady}
           theme={{ ...themeAbyssSpaced, gap: 5 }}
           components={CardComponentMap}
           leftHeaderActionsComponent={DashboardPlus}

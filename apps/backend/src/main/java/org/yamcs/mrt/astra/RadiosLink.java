7c2a44bdbc6d396d79b1b957104c6d91bd6c00b0
Léo Mindlin <hello@leomindlin.com>
2026-02-28 22:11:37 -0500

    oxlint too

--- apps/backend/src/main/java/org/yamcs/mrt/astra/RadiosLink.java
+++ apps/backend/src/main/java/org/yamcs/mrt/astra/RadiosLink.java
@@ -13,54 +13,53 @@ import org.yamcs.mrt.MqttToTmPacketConverter;
 import org.yamcs.tctm.Link;
 
 public class RadiosLink extends AstraSubLink {
-	MqttToTmPacketConverter tmConverter;
-	private String deviceName;
-	private String deviceFrequency;
-	private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+  MqttToTmPacketConverter tmConverter;
+  private String deviceName;
+  private String deviceFrequency;
+  private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
 
-	public RadiosLink(MqttAsyncClient client, String frequency) {
-		super(client);
-		this.deviceFrequency = frequency;
-	}
+  public RadiosLink(MqttAsyncClient client, String frequency) {
+    super(client);
+    this.deviceFrequency = frequency;
+  }
 
-	@Override
-	public void init(String yamcsInstance, String linkName, YConfiguration config)
-			throws ConfigurationException {
-		super.init(yamcsInstance, linkName, config);
+  @Override
+  public void init(String yamcsInstance, String linkName, YConfiguration config)
+      throws ConfigurationException {
+    super.init(yamcsInstance, linkName, config);
 
-		this.deviceName = linkName.split("/")[1];
+    this.deviceName = linkName.split("/")[1];
 
-		tmConverter = new DefaultMqttToTmPacketConverter();
-		tmConverter.init(yamcsInstance, linkName, config);
-	}
+    tmConverter = new DefaultMqttToTmPacketConverter();
+    tmConverter.init(yamcsInstance, linkName, config);
+  }
 
-	@Override
-	public void handleMqttMessage(MqttMessage message) {
-		dataIn(1, message.getPayload().length);
+  @Override
+  public void handleMqttMessage(MqttMessage message) {
+    dataIn(1, message.getPayload().length);
 
-		// This is the only point where we can access the binary packet
-		// In order to pass acks along to toe AstraCommandLinkClass
-		byte flags = message.getPayload()[2];
-		boolean ackFlag = ((flags >> 1) & 1) == 1;
-		if (ackFlag) {
-			byte commandAckIdByte = message.getPayload()[3];
-			int commandAckId = commandAckIdByte & 0xFF;
+    // This is the only point where we can access the binary packet
+    // In order to pass acks along to toe AstraCommandLinkClass
+    byte flags = message.getPayload()[2];
+    boolean ackFlag = ((flags >> 1) & 1) == 1;
+    if (ackFlag) {
+      byte commandAckIdByte = message.getPayload()[3];
+      int commandAckId = commandAckIdByte & 0xFF;
 
-			var links = YamcsServer.getServer().getInstance(this.getYamcsInstance()).getLinkManager().getLinks();
-			for (Link link : links) {
-				if (link instanceof AstraCommandLink) {
-					((AstraCommandLink) link).handleFCAck(commandAckId, deviceFrequency, deviceName);
-				}
-			}
-		}
+      var links =
+          YamcsServer.getServer().getInstance(this.getYamcsInstance()).getLinkManager().getLinks();
+      for (Link link : links) {
+        if (link instanceof AstraCommandLink) {
+          ((AstraCommandLink) link).handleFCAck(commandAckId, deviceFrequency, deviceName);
+        }
+      }
+    }
 
-		for (var tmPacket : tmConverter.convert(message)) {
-			tmPacket = packetPreprocessor.process(tmPacket);
-			if (tmPacket != null) {
-				super.processPacket(tmPacket);
-			}
-		}
-
-	}
-
+    for (var tmPacket : tmConverter.convert(message)) {
+      tmPacket = packetPreprocessor.process(tmPacket);
+      if (tmPacket != null) {
+        super.processPacket(tmPacket);
+      }
+    }
+  }
 }

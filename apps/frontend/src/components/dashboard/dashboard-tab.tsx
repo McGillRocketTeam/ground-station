69d7d4a6ca369c2d712769c98b4aecbc72ec5202
Léo Mindlin <hello@leomindlin.com>
2026-03-05 19:33:08 -0500

    context menu on tabs

--- apps/frontend/src/components/dashboard/dashboard-tab.tsx
+++ apps/frontend/src/components/dashboard/dashboard-tab.tsx
@@ -1,9 +1,22 @@
 import type { IDockviewDefaultTabProps } from "dockview-react";
 
+import {
+  ContextMenu,
+  ContextMenuContent,
+  ContextMenuItem,
+  ContextMenuTrigger,
+} from "@/components/ui/context-menu";
+
 export function DashboardTab(props: IDockviewDefaultTabProps) {
   return (
-    <div className="grid h-full place-items-center px-1">
-      {props.api.title ?? "New Tab"}
-    </div>
+    <ContextMenu>
+      <ContextMenuTrigger className="grid h-full place-items-center px-1">
+        {props.api.title ?? "New Tab"}
+      </ContextMenuTrigger>
+      <ContextMenuContent>
+        <ContextMenuItem>Edit</ContextMenuItem>
+        <ContextMenuItem variant="destructive">Delete</ContextMenuItem>
+      </ContextMenuContent>
+    </ContextMenu>
   );
 }
